7b6bb282753ae7df075b64b2d51fdafc10f8153c
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-19 19:08:04 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,14 @@
 require 'net/http'
+require 'uri'
 require 'json'
 
-res = Net::HTTP.post_form URI ('https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en'), 
-{'names'=>'TTP-2B'}
+#uri = URI ('https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en')
+#params = {:names => 'TTP-2B'}
+#uri.query = URI.encode_www_form(params)
+#res = Net::HTTP.get_response (uri)
+#puts res.body
+
+
+uri = URI ('https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en')
+res = Net::HTTP.post_form(uri, ['names':'TTP-2B'])
 puts res.body
