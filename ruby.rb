9a2361a61afe52f6acc0f5405cee2685be4f6677
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-19 14:39:03 +0400

    Add json

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,6 @@
-puts 1+1
+require 'net/http'
+require 'json'
+
+uri = URI ('https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en')
+res = Net::HTTP.post_form(uri, ["TTP-2B"])
+puts res.body
