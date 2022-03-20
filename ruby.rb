81891d41041749cb0bb2332ef07d8823a36fc176
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-20 11:17:46 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,22 @@
+#подключаем внешний руби-код для методов JSON, net/http и uri
 require 'net/http'
 require 'uri'
 require 'json'
 
-res = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
- "[\"TTP-2B\"]", "Content-Type" => "application/json") # вместо "[\"TTP-2B\"]" можно ["TTP-2B"].to_json
-puts res.body
+result = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
+["TTP-2B"].to_json) #получаем данные с сайта в формате json
+result=JSON.parse(result.body) #преобразуем JSON в хеш
+
+#result=result.values_at("systems\\")
+
+#result=result.body.inspect
+
+ 
+puts result
+
+
+
+ #result.inspect превращает хеш в строку
+
+ #{"systems":[{"id":30000812,"name":"TTP-2B"}]}
+ #добавить проверку если хеш не содержит "systems": значит это не система и выводить ошибку
