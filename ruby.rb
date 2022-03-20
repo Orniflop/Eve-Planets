93318bbc7e4792c51918dc69678ec6b9a2d130b1
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-20 11:39:47 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -6,15 +6,15 @@ require 'json'
 result = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
 ["TTP-2B"].to_json) #получаем данные с сайта в формате json
 result=JSON.parse(result.body) #преобразуем JSON в хеш
+puts "Такой системы нет!" if !(result.key?("systems")) #проверяем что это действительно солнечная система
 
-#result=result.values_at("systems\\")
-
-#result=result.body.inspect
+#result=result.inspect
 
  
 puts result
 
 
+#result.values_at("systems")
 
  #result.inspect превращает хеш в строку
 
