fd8d0ca8bc4dadba321f25f34c628e25f1705632
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-21 15:24:26 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -9,9 +9,8 @@ require 'json'
 system_name=gets.chomp #запрашиваем у пользователя имя системы и обрезаем ей \n
 
 #получаем данные с сайта в формате json и преобразуем их в хеш
-result = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
-["#{system_name}"].to_json)
-result = JSON.parse(result.body)
+result = JSON.parse(Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
+["#{system_name}"].to_json).body)
 
 #проверяем что это действительно солнечная система, иначе прерываем программу
 if !(result.key?("systems"))
@@ -19,15 +18,12 @@ if !(result.key?("systems"))
     abort
 end
 
-#получаем из хеша значение ключа, пример для TTP-2B: {"systems":[{"id":30000812,"name":"TTP-2B"}]}
-result = result["systems"] #получаем [{"id":30000812,"name":"TTP-2B"}]
-result = result[0] #получаем {"id":30000812,"name":"TTP-2B"}
-system_id = result["id"] #получаем 30000812
+#получаем из хеша значение id, пример для TTP-2B: {"systems":[{"id":30000812,"name":"TTP-2B"}]}
+system_id = result.dig('systems', 0, 'id') #получаем 30000812
 
-#получаем информацию о системе по ее id
-result = Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/systems/#{system_id}/?datasource=tranquility&language=en"))
-result = JSON.parse(result)
-result = result["planets"] #получаем массив хешей с планетами системы
+#получаем информацию о системе по ее id и из нее выбираем в массив хеши с планетами системы
+result = JSON.parse(Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/systems/#{system_id}/?datasource=tranquility&language=en")))
+result = result["planets"]
 
 #итерация массива для выбора всех planet_id и помещение их в массив
 planets_id=[]
@@ -38,8 +34,7 @@ end
 #получаем сведения по каждой планете и помещаем в массив ее name и type_id
 planets=[]
 planets_id.each do |planet_id|
-    result = Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/planets/#{planet_id}/?datasource=tranquility"))
-    result = JSON.parse(result)
+    result = JSON.parse(Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/planets/#{planet_id}/?datasource=tranquility")))
     result = result.values_at("name","type_id")
     planets<<result
 end
@@ -48,8 +43,7 @@ planets = planets.to_h #преобразуем массив в хеш
 
 #по значению хеша получаем тип планеты (например "Planet (Gas)"), обрезаем лишнее и в хеше меняем значение на тип планеты 
 planets.each do |key, value|
-    result = Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/types/#{value}/?datasource=tranquility&language=en"))
-    result = JSON.parse(result)
+    result = JSON.parse(Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/types/#{value}/?datasource=tranquility&language=en")))
     result = result["name"].chop.sub("Planet (", "") #берем из хеша значение и обрезаем ) и Planet (
     planets[key] = result
 end
