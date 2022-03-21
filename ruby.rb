e82d0b70b6bbcfe8d5bc6b28aa8aa9ce120771a1
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-21 11:55:04 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -1,16 +1,22 @@
+#задача: пользователь вводит название системы из вселенной игры eve-online, программа выдает сведения о начилии
+#планет в системе (название и тип планеты). На примере системы TTP-2B.
+
 #подключаем внешний руби-код для методов JSON, net/http и uri
 require 'net/http'
 require 'uri'
 require 'json'
 
-system_name=gets.chomp #запрашиваем имя системы и обрезаем ей \n
-result = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
-["#{system_name}"].to_json) #получаем данные с сайта в формате json
-result = JSON.parse(result.body) #преобразуем JSON в хеш
+system_name=gets.chomp #запрашиваем у пользователя имя системы и обрезаем ей \n
 
-if !(result.key?("systems")) #проверяем что это действительно солнечная система
+#получаем данные с сайта в формате json и преобразуем их в хеш
+result = Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),
+["#{system_name}"].to_json)
+result = JSON.parse(result.body)
+
+#проверяем что это действительно солнечная система, иначе прерываем программу
+if !(result.key?("systems"))
     puts "Такой системы нет!"
-    abort #прерываем программу
+    abort
 end
 
 #получаем из хеша значение ключа, пример для TTP-2B: {"systems":[{"id":30000812,"name":"TTP-2B"}]}
@@ -40,12 +46,11 @@ end
 
 planets = planets.to_h #преобразуем массив в хеш
 
-#по значению хеша получаем тип планеты ("Planet (Gas)"), обрезаем лишнее и в хеше меняем значение на тип планеты 
+#по значению хеша получаем тип планеты (например "Planet (Gas)"), обрезаем лишнее и в хеше меняем значение на тип планеты 
 planets.each do |key, value|
     result = Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/types/#{value}/?datasource=tranquility&language=en"))
     result = JSON.parse(result)
-    result = result["name"]
-    result = result.chop.sub("Planet (", "") #обрезаем ) и "Planet (
+    result = result["name"].chop.sub("Planet (", "") #берем из хеша значение и обрезаем ) и Planet (
     planets[key] = result
 end
 
