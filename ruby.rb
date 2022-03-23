387edc736942d2bcd8b8e6eaba6acb368f5a0153
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-23 09:50:10 +0400

    Update ruby.rb

--- ruby.rb
+++ ruby.rb
@@ -34,8 +34,6 @@ planets.each_pair do |key, value|
     planets[key] = JSON.parse(Net::HTTP.get(URI("https://esi.evetech.net/latest/universe/types/#{value}/?datasource=tranquility&language=en")))["name"].chop.sub("Planet (", "")
 end
 
-puts planets
-
 #добавляем все планетарные ресурсы в игре
 Raw_Resource={"Aqueous Liquids":["Barren","Storm","Temperate","Ice","Gas","Oceanic"],"Autotrophs":["Temperate"],"Base Metals":["Barren","Storm","Gas","Plasma","Lava"],"Carbon Compounds":["Barren","Temperate","Oceanic"],"Complex Organisms":["Temperate","Oceanic"],"Felsic Magma":["Lava"],"Heavy Metals":["Ice","Plasma","Lava"],"Ionic Solutions":["Storm","Gas"],"Microorganisms":["Barren","Temperate","Ice","Oceanic"],"Noble Gas":["Storm","Ice","Gas"],"Noble Metals":["Barren","Plasma"],"Non-CS Crystals":["Plasma","Lava"],"Planktic Colonies":["Ice","Oceanic"],"Reactive Gas":["Gas"],"Suspended Plasma":["Storm","Plasma","Lava"]}
 Tier1={"Aqueous Liquids":["Water"],"Autotrophs":["Industrial Fibers"],"Base Metals":["Reactive Metals"],"Carbon Compounds":["Biofuels"],"Complex Organisms":["Proteins"],"Felsic Magma":["Silicon"],"Heavy Metals":["Toxic Metals"],"Ionic Solutions":["Electrolytes"],"Microorganisms":["Bacteria"],"Noble Gas":["Oxygen"],"Noble Metals":["Precious Metals"],"Non-CS Crystals":["Chiral Structures"],"Planktic Colonies":["Biomass"],"Reactive Gas":["Oxidizing Compound"],"Suspended Plasma":["Plasmoids"]}
@@ -43,4 +41,15 @@ Tier2={"Biocells":["Biofuels","Precious Metals"],"Construction Blocks":["Toxic M
 Tier3={"Biotech Research Reports":["Construction Blocks","Livestock","Nanites"],"Camera Drones":["Rocket Fuel","Silicate Glass"],"Condensates":["Coolant","Oxides"],"Cryoprotectant Solution":["Synthetic Oil","Fertilizer","Test Cultures"],"Data Chips":["Supertensile Plastics","Microfiber Shielding"],"Gel-Matrix Biopaste":["Superconductors","Biocells","Oxides"],"Guidance Systems":["Water-Cooled CPU","Transmitter"],"Hazmat Detection Systems":["Transmitter","Viral Agent","Polytextiles"],"Hermetic Membranes":["Polyaramids","Genetically Enhanced Livestock"],"High-Tech Transmitters":["Transmitter","Polyaramids"],"Industrial Explosives":["Fertilizer","Polytextiles"],"Neocoms":["Biocells","Silicate Glass"],"Nuclear Reactors":["Enriched Uranium","Microfiber Shielding"],"Planetary Vehicles":["Supertensile Plastics","Miniature Electronics","Mechanical Parts"],"Robotics":["Consumer Electronics","Mechanical Parts"],"Smartfab Units":["Miniature Electronics","Construction Blocks"],"Supercomputers":["Water-Cooled CPU","Coolant","Consumer Electronics"],"Synthetic Synapses":["Supertensile Plastics","Test Cultures"],"Transcranial Microcontrollers":["Biocells","Nanites"],"Ukomi Superconductors":["Superconductors","Synthetic Oil"],"Vaccines":["Livestock","Viral Agent"]}
 Tier4={"Broadcast Node":["Data Chips","Neocoms","High-Tech Transmitters"],"Integrity Response Drones":["Gel-Matrix Biopaste","Planetary Vehicles","Hazmat Detection Systems"],"Nano-Factory":["Industrial Explosives","Ukomi Superconductors","Reactive Metals"],"Organic Mortar Applicators":["Condensates","Robotics","Bacteria"],"Recursive Computing Module":["Synthetic Synapses","Transcranial Microcontrollers","Guidance Systems"],"Self-Harmonizing Power Core":["Nuclear Reactors","Camera Drones","Hermetic Membranes"],"Sterile Conduits":["Vaccines","Water","Smartfab Units"],"Wetware Mainframe":["Biotech Research Reports","Supercomputers","Cryoprotectant Solution"]}
 
+#проверяем какие Raw_Resource можно добывать в системе
+system_Raw_Resource=[]
+planets.each do |value1|
+    Raw_Resource.each_pair do |key, value2|
+        system_Raw_Resource<<key if value1=value2
+    end
+end
+system_Raw_Resource=system_Raw_Resource.uniq
 
+#выводим виды планет и ресурсов в заданной системе
+puts planets
+puts "Базовые ресурсы: #{system_Raw_Resource.join(', ')}" #вывод массива в строку через запятую
