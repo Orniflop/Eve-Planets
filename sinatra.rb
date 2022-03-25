75c9ea2b8fc2196d1ce0e56f53e7d5f020be2c97
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-25 17:15:52 +0400

    Update sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'json'
 
 get '/' do
+    erb :index
+end
+
+post '/' do
+    @system_name = params[:system_name]
+    @result = JSON.parse(Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),["#{@system_name}"].to_json).body)
     erb :index
 end
