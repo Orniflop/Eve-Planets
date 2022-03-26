de58d0ac65ac6233e4f7a23777f21951bae67d91
Orniflop <101870927+Orniflop@users.noreply.github.com>
2022-03-26 12:01:13 +0400

    Update sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -9,6 +9,15 @@ end
 
 post '/' do
     @system_name = params[:system_name]
-    @result = JSON.parse(Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),["#{@system_name}"].to_json).body)
+    result = JSON.parse(Net::HTTP.post(URI("https://esi.evetech.net/latest/universe/ids/?datasource=tranquility&language=en"),["#{@system_name}"].to_json).body)
+    
+    abort @no_system="Такой системы нет!" unless result.key?("systems")
+    
+    system_id = result.dig('systems', 0, 'id')
+    
+    
+    
+    
+    
     erb :index
 end
